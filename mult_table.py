a88f2b49bfd53823c3ab5d88d2bb8c3c140f37ee
Lluc <llucroig2001@gmail.com>
2022-02-27 19:28:30 +0100

    Making table

---
+++ mult_table.py
@@ -0,0 +1,8 @@
+"""
+Crea un programa que muestre por pantalla la tabla de multiplicar introducida por el usuario del 5 al 15
+"""
+
+n_tabla = int(input("Introduce un número: "))
+
+for num in range(5,16):
+    print("{} x {} = {}".format(n_tabla,num,n_tabla * num))
